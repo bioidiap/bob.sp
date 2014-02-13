25ef2ed6b7e74579210bbda56c23d40f95a5c7e1
Andre Anjos <andre.anjos@idiap.ch>
2014-02-13 16:59:42 +0100

    Add convenience functions

--- xbob/sp/main.cpp
+++ xbob/sp/main.cpp
@@ -23,7 +23,7 @@ extern PyTypeObject PyBobSpExtrapolationBorder_Type;
 extern PyTypeObject PyBobSpQuantization_Type;
 
 PyDoc_STRVAR(s_extrapolate_str, "extrapolate");
-PyDoc_STRVAR(s_extrapolate__doc__,
+PyDoc_STRVAR(s_extrapolate_doc,
 "extrapolate(src, dst, [[border=" XBOB_EXT_MODULE_PREFIX ".BorderSize.Zero], value=0.]) -> None\n\
 \n\
 Extrapolates values in the given array using the specified border\n\
@@ -36,12 +36,202 @@ as input. For example, ``False``, if the input is boolean and\n\
 ");
 PyObject* extrapolate(PyObject*, PyObject* args, PyObject* kwds);
 
+PyDoc_STRVAR(s_fft_str, "fft");
+PyDoc_STRVAR(s_fft_doc,
+"fft(src, [dst]) -> array\n\
+\n\
+Computes the direct Fast Fourier Transform of a 1D or 2D\n\
+array/signal of type ``complex128``. Allocates a new output\n\
+array if ``dst`` is not provided. If it is, then it must\n\
+be of the same type and shape as ``src``.\n\
+\n\
+Parameters:\n\
+\n\
+src\n\
+  [array] A 1 or 2-dimensional array of type ``complex128``\n\
+  in which the FFT operation will be performed.\n\
+\n\
+dst\n\
+  [array, optional] A 1 or 2-dimensional array of type\n\
+  ``complex128`` and matching dimensions to ``src`` in\n\
+  which the result of the operation will be stored.\n\
+\n\
+Returns a 1 or 2-dimensional array, of the same dimension\n\
+as ``src``, of type ``complex128``, containing the FFT of\n\
+the input signal.\n\
+");
+PyObject* fft(PyObject*, PyObject* args, PyObject* kwds);
+
+PyDoc_STRVAR(s_ifft_str, "ifft");
+PyDoc_STRVAR(s_ifft_doc,
+"ifft(src, [dst]) -> array\n\
+\n\
+Computes the inverse Fast Fourier Transform of a 1D or 2D\n\
+transform of type ``complex128``. Allocates a new output\n\
+array if ``dst`` is not provided. If it is, then it must\n\
+be of the same type and shape as ``src``.\n\
+\n\
+Parameters:\n\
+\n\
+src\n\
+  [array] A 1 or 2-dimensional array of type ``complex128``\n\
+  in which the inverse FFT operation will be performed.\n\
+\n\
+dst\n\
+  [array, optional] A 1 or 2-dimensional array of type\n\
+  ``complex128`` and matching dimensions to ``src`` in\n\
+  which the result of the operation will be stored.\n\
+\n\
+Returns a 1 or 2-dimensional array, of the same dimension\n\
+as ``src``, of type ``complex128``, containing the inverse\n\
+FFT of the input transform.\n\
+");
+PyObject* ifft(PyObject*, PyObject* args, PyObject* kwds);
+
+PyDoc_STRVAR(s_fftshift_str, "fftshift");
+PyDoc_STRVAR(s_fftshift_doc,
+"fftshift(src, [dst]) -> array\n\
+\n\
+If a 1D complex128 array is passed, inverses the two halves\n\
+of that array and returns the result as a new array. If a 2D\n\
+``complex128`` array is passed, swaps the four quadrants of\n\
+the array and returns the result as a new array.\n\
+\n\
+Parameters:\n\
+\n\
+src\n\
+[array] A 1 or 2-dimensional array of type ``complex128``\n\
+with the signal to be shifted.\n\
+\n\
+dst\n\
+[array, optional] A 1 or 2-dimensional array of type\n\
+``complex128`` and matching dimensions to ``src`` in\n\
+which the result of the operation will be stored.\n\
+\n\
+Returns a 1 or 2-dimensional array, of the same dimension\n\
+as ``src``, of type ``complex128``, containing the shifted\n\
+version of theinput.\n\
+");
+PyObject* fftshift(PyObject*, PyObject* args, PyObject* kwds);
+
+PyDoc_STRVAR(s_ifftshift_str, "ifftshift");
+PyDoc_STRVAR(s_ifftshift_doc,
+"ifftshift(src, [dst]) -> array\n\
+\n\
+This method undoes what :py:meth:`fftshift` does. It accepts\n\
+1 or 2-dimensional arrays of type ``complex128``.\n\
+\n\
+Parameters:\n\
+\n\
+src\n\
+[array] A 1 or 2-dimensional array of type ``complex128``\n\
+with the signal to be shifted.\n\
+\n\
+dst\n\
+[array, optional] A 1 or 2-dimensional array of type\n\
+``complex128`` and matching dimensions to ``src`` in\n\
+which the result of the operation will be stored.\n\
+\n\
+Returns a 1 or 2-dimensional array, of the same dimension\n\
+as ``src``, of type ``complex128``, containing the shifted\n\
+version of theinput.\n\
+");
+PyObject* ifftshift(PyObject*, PyObject* args, PyObject* kwds);
+
+PyDoc_STRVAR(s_dct_str, "dct");
+PyDoc_STRVAR(s_dct_doc,
+"dct(src, [dst]) -> array\n\
+\n\
+Computes the direct Discrete Cosine Transform of a 1D or 2D\n\
+array/signal of type ``float64``. Allocates a new output\n\
+array if ``dst`` is not provided. If it is, then it must\n\
+be of the same type and shape as ``src``.\n\
+\n\
+Parameters:\n\
+\n\
+src\n\
+  [array] A 1 or 2-dimensional array of type ``float64``\n\
+  in which the DCT operation will be performed.\n\
+\n\
+dst\n\
+  [array, optional] A 1 or 2-dimensional array of type\n\
+  ``float64`` and matching dimensions to ``src`` in\n\
+  which the result of the operation will be stored.\n\
+\n\
+Returns a 1 or 2-dimensional array, of the same dimension\n\
+as ``src``, of type ``float64``, containing the DCT of\n\
+the input signal.\n\
+");
+PyObject* dct(PyObject*, PyObject* args, PyObject* kwds);
+
+PyDoc_STRVAR(s_idct_str, "idct");
+PyDoc_STRVAR(s_idct_doc,
+"idct(src, [dst]) -> array\n\
+\n\
+Computes the inverse Discrete Cosinte Transform of a 1D or 2D\n\
+transform of type ``float64``. Allocates a new output\n\
+array if ``dst`` is not provided. If it is, then it must\n\
+be of the same type and shape as ``src``.\n\
+\n\
+Parameters:\n\
+\n\
+src\n\
+  [array] A 1 or 2-dimensional array of type ``float64``\n\
+  in which the inverse DCT operation will be performed.\n\
+\n\
+dst\n\
+  [array, optional] A 1 or 2-dimensional array of type\n\
+  ``float64`` and matching dimensions to ``src`` in\n\
+  which the result of the operation will be stored.\n\
+\n\
+Returns a 1 or 2-dimensional array, of the same dimension\n\
+as ``src``, of type ``float64``, containing the inverse\n\
+DCT of the input transform.\n\
+");
+PyObject* idct(PyObject*, PyObject* args, PyObject* kwds);
+
 static PyMethodDef module_methods[] = {
     {
       s_extrapolate_str,
       (PyCFunction)extrapolate,
       METH_VARARGS|METH_KEYWORDS,
-      s_extrapolate__doc__
+      s_extrapolate_doc
+    },
+    {
+      s_fft_str,
+      (PyCFunction)fft,
+      METH_VARARGS|METH_KEYWORDS,
+      s_fft_doc
+    },
+    {
+      s_ifft_str,
+      (PyCFunction)ifft,
+      METH_VARARGS|METH_KEYWORDS,
+      s_ifft_doc
+    },
+    {
+      s_fftshift_str,
+      (PyCFunction)fftshift,
+      METH_VARARGS|METH_KEYWORDS,
+      s_fftshift_doc
+    },
+    {
+      s_ifftshift_str,
+      (PyCFunction)ifftshift,
+      METH_VARARGS|METH_KEYWORDS,
+      s_ifftshift_doc
+    },
+    {
+      s_dct_str,
+      (PyCFunction)dct,
+      METH_VARARGS|METH_KEYWORDS,
+      s_dct_doc
+    },
+    {
+      s_idct_str,
+      (PyCFunction)idct,
+      METH_VARARGS|METH_KEYWORDS,
+      s_idct_doc
     },
     {0}  /* Sentinel */
 };
@@ -54,7 +244,7 @@ static PyModuleDef module_definition = {
   XBOB_EXT_MODULE_NAME,
   module_docstr,
   -1,
-  module_methods, 
+  module_methods,
   0, 0, 0, 0
 };
 #endif
